99a989409e9c2fe74cf3d72551da43684fcf1015
Logan Waite <lwaite2010@gmail.com>
2019-05-22 16:20:11 -0600

    get ramda working (which was weird) and convert Main to funcional component

--- src/components/Main/Card/index.js
+++ src/components/Main/Card/index.js
@@ -1,15 +1,19 @@
-import React from "react";
-import styles from "./styles";
+import React from 'react'
+import styles from './styles'
 
 const Card = props => {
-    return (
-        <div style={{ ...styles.container, ...props.style }}>
-            <div style={styles.name}>{props.name}</div>
-            <img src={props.image} style={styles.playerImage} alt="player_image" />
+  return (
+    <div style={{ ...styles.container, ...props.style }}>
+      <div style={styles.name}>{props.name}</div>
+      <img
+        src={`http://localhost:3008/${props.image}`}
+        style={styles.playerImage}
+        alt='player_image'
+      />
 
-            <div>{props.team}</div>
-        </div>
-    );
-};
+      <div>{props.team}</div>
+    </div>
+  )
+}
 
-export default Card;
+export default Card
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import * as R from 'ramda'
 import Search from './Search'
 import Card from './Card'
 import styles from './styles'
@@ -9,16 +10,14 @@ const getPlayers = () => {
   )
 }
 
-class Main extends Component {
-  render () {
-    return (
-      <div style={{ ...styles.container, ...this.props.style }}>
-        <div style={styles.title}>NBA Interview</div>
-        <Search style={styles.search} />
-        <Card />
-      </div>
-    )
-  }
+const Main = props => {
+  return (
+    <div style={{ ...styles.container, ...props.style }}>
+      <div style={styles.title}>NBA Interview</div>
+      <Search style={styles.search} />
+      <Card />
+    </div>
+  )
 }
 
 export default Main
