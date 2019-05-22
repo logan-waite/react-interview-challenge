5884f58550d915d9893848222e0f2482b93c89a5
Logan Waite <lwaite2010@gmail.com>
2019-05-22 15:45:36 -0600

    add props for name, image and team to card

--- src/components/Main/Card/index.js
+++ src/components/Main/Card/index.js
@@ -4,10 +4,10 @@ import styles from "./styles";
 const Card = props => {
     return (
         <div style={{ ...styles.container, ...props.style }}>
-            <div style={styles.name}>Gordon "Snake" Hayward</div>
-            <img src="http://localhost:3008/gordon_hayward.png" style={styles.playerImage} alt="player_image" />
+            <div style={styles.name}>{props.name}</div>
+            <img src={props.image} style={styles.playerImage} alt="player_image" />
 
-            <div>Boston Celtics</div>
+            <div>{props.team}</div>
         </div>
     );
 };
