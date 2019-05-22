dd52cf37f106ef3f9040ffdbee51b447a3e1e011
Logan Waite <lwaite2010@gmail.com>
2019-05-22 17:09:00 -0600

    gets list of teams and apply them to players

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,14 +10,56 @@ const getPlayers = () => {
   )
 }
 
-const Main = props => {
-  return (
-    <div style={{ ...styles.container, ...props.style }}>
-      <div style={styles.title}>NBA Interview</div>
-      <Search style={styles.search} />
-      <Card />
-    </div>
+const getTeams = () => {
+  return fetch('http://localhost:3008/teams').then(response => response.json())
+}
+
+const getByValue = R.curry((value, prop, list) =>
+  R.find(item => R.equals(R.prop(prop, item), value), list)
+)
+
+const getPlayerInfo = async () => {
+  const players = await getPlayers()
+  const teams = await getTeams()
+  return R.map(
+    player =>
+      R.assoc('team', getByValue(player.team, 'id', teams).name, player),
+    players
   )
+}
+
+class Main extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount () {
+    getPlayerInfo().then(result => this.setState({ players: result }))
+  }
+
+  render () {
+    return (
+      <div style={{ ...styles.container, ...this.props.style }}>
+        <div style={styles.title}>NBA Interview</div>
+        <Search style={styles.search} />
+        {R.map(
+          player => (
+            <Card
+              key={player.id}
+              name={player.name}
+              image={player.image}
+              team={player.team}
+            />
+          ),
+          this.state.players
+        )}
+      </div>
+    )
+  }
 }
 
 export default Main
