a7ec115bc920a0767930e859939e9a57fc4e1491
Logan Waite <lwaite2010@gmail.com>
2019-05-22 20:02:49 -0600

    add free fontawesome icons

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import styles from './styles'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as hollowHeart } from '@fortawesome/free-regular-svg-icons'
 
 const EditButton = ({ onClick }) => <button onClick={onClick}>Edit</button>
 
 const Card = props => {
   return (
     <div style={{ ...styles.container, ...props.style }}>
+      <FontAwesomeIcon icon={props.isFavorite ? solidHeart : hollowHeart} />
       <div style={styles.name}>{props.name}</div>
       <img
         src={`http://localhost:3008/${props.image}`}
